1372b2bb32ce6282ed4139fc3e68f872c8342700
Jason Flaherty <flaherty.jason@gmail.com>
2021-06-03 22:33:26 +0200

    sequential server

---
+++ utils.h
@@ -0,0 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+// Reports a peer connection to stdout. sa is the data populated by a successful
+// accept() call.
+void report_peer_connected(const struct sockaddr_in* sa, socklen_t salen);
+
+// Creates a bound and listening INET socket on the given port number. Returns
+// the socket fd when successful; dies in case of errors.
+int listen_inet_socket(int portnum);
+
+#endif
